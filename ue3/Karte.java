c09e2a5f70c785d855fe4bb27fadd74607490b1d
Partero1 <74548973+Partero1@users.noreply.github.com>
2020-12-01 18:05:47 +0100

    Deck fertig

--- ue3/Karte.java
+++ ue3/Karte.java
@@ -3,71 +3,70 @@ public class Karte {
     public Wert wert;
 
 
-public String toString(Karte karte){
-    String ausgabe = "";
-    String ausFarbe = farbe.toString();
-    String ausWert = wert.toString();
-    ausgabe = ausFarbe + ausWert;
-    return ausgabe;
-}
+    public String toString(){
+        return farbe.toString() +  wert.toString();
+    }
 
-public static Karte neueKarte(Farbe f, Wert w){
-Karte ausgabe = new Karte();
-ausgabe.farbe = f;
-ausgabe.wert = w;
-return ausgabe;
-}
-
-public static Karte neueKarte(String f, String w){
+    public static Karte neueKarte(Farbe f, Wert w){
     Karte ausgabe = new Karte();
-    Farbe fa = Farbe.HERZ;
-    Wert we = Wert.ASS;
-    if(f.equals("HERZ")){ 
-        fa = Farbe.HERZ;}
-    else{
-        if(f.equals("KARO")){ 
-            fa = Farbe.KARO;}
-        else{
-            if(f.equals("PIKE")){ 
-                fa = Farbe.PIKE;}
-            else{
-                if(f.equals("KREUZ")){ 
-                    fa = Farbe.KREUZ;}               
-            }
-        }
-    }
-
-    if(w.equals("ACHT")){ 
-        we = Wert.ACHT;}
-    else{
-        if(w.equals("NEUN")){ 
-            we = Wert.NEUN;}
-        else{
-            if(w.equals("ZEHN")){ 
-                we = Wert.ZEHN;}
-            else{
-                if(f.equals("BUBE")){ 
-                    we = Wert.BUBE;} 
-                else{
-                    if(f.equals("DAME")){ 
-                        we = Wert.DAME;} 
-                    else{
-                        if(f.equals("KOENIG")){ 
-                            we = Wert.KOENIG;} 
-                        else{
-                            if(f.equals("ASS")){ 
-                                we = Wert.ASS;}                            
-                        } 
-                    } 
-                }              
-            }
-        }
-    }
-
-
-    ausgabe.farbe = fa;
-    ausgabe.wert = we;
+    ausgabe.farbe = f;
+    ausgabe.wert = w;
     return ausgabe;
+    }
+
+    public static Karte neueKarte(String f, String w){
+        Karte ausgabe = new Karte();
+        Farbe fa = Farbe.HERZ;
+        Wert we = Wert.ASS;
+      
+        fa = switch (f) {
+            case "HERZ" -> Farbe.HERZ;
+            case "KARO" -> Farbe.KARO;
+            case "PIKE" -> Farbe.PIKE;
+            case "KREUZ" -> Farbe.KREUZ;
+            default -> Farbe.HERZ;
+        }; 
+
+        we = switch (w) {
+            case "SIEBEN" -> Wert.SIEBEN;
+            case "ACHT" -> Wert.ACHT;
+            case "NEUN" -> Wert.NEUN;
+            case "ZEHN" -> Wert.ZEHN;
+            case "BUBE" -> Wert.BUBE;
+            case "DAME" -> Wert.DAME;
+            case "KOENIG" -> Wert.KOENIG;
+            case "ASS" -> Wert.ASS;
+            default -> Wert.ASS;
+        }; 
+
+
+        ausgabe.farbe = fa;
+        ausgabe.wert = we;
+        return ausgabe;
+    }
+
+    public static int kombinationen(){
+        return Farbe.values().length * Wert.values().length;
+    }
+    
+
+    public static Karte[] skatblatt(){
+        int kombo = kombinationen();
+        Karte [] deck = new Karte [kombo];
+        int i = 0;
+
+        for(Farbe p1 : Farbe.values()){
+            for(Wert p : Wert.values()){
+                Karte b = neueKarte(p1, p);
+                deck[i] = b;
+                i++;
+            }
+
+        }
+        
+
+
+        return deck;
     }
 
 }
--- ue3/MauMau.java
+++ ue3/MauMau.java
@@ -2,12 +2,9 @@ public class MauMau {
 
     
 public static void main(String[] args){
-    Farbe f1 = Farbe.KREUZ;
-    Wert w1 = Wert.ACHT;
-    Karte k1 = Karte.neueKarte(f1, w1);
-   
-    String ausgabe = k1.toString();
-    java.lang.System.out.print(ausgabe);
-    java.lang.System.out.print("ausgabe");
+    Karte [] p = Karte.skatblatt();
+
+    int i = 2;
+    i++;
 }
 }
