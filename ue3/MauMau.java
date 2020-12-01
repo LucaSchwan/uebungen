5facac515c74b4348616384951ce28cb5e9b82a5
Partero1 <74548973+Partero1@users.noreply.github.com>
2020-12-01 20:32:36 +0100

    h)

--- ue3/MauMau.java
+++ ue3/MauMau.java
@@ -2,7 +2,16 @@ public class MauMau {
 
     
 public static void main(String[] args){
-  Karte.druckeEinbahnBedienungen();
- int i =0 ;
+   Spieler test = new Spieler();   
+   test.gewinnProzent = 12.5;
+   Spieler test2 = new Spieler();   
+   test2.gewinnProzent = 2.8;
+   Spieler test3 = new Spieler();   
+   test3.gewinnProzent = 2.0;
+   Spieler test4 = new Spieler();   
+   test4.gewinnProzent = 4.6;
+  
+    Spieler.besterSpieler(test, test2, test3, test4);
+    
 }
 }
--- ue3/Spieler.java
+++ ue3/Spieler.java
@@ -10,8 +10,39 @@ public class Spieler {
 
     public static Spieler besterSpieler(Spieler...club){
         Spieler test = new Spieler();
+        double[] g = new double[club.length];
+        for(int i = 0; i < club.length; i++){
+            g[i] = club[i].gewinnProzent;
+        }
+        sort(g);
+        System.out.println(g);
+        for(int o = 0; o < club.length; o++){
+            if(club[o].gewinnProzent == g[0]){
+                return club[0];
+            }
+        }
         return test;
     }
 
+
+    public static void sort(double[] a) {
+		//int [] a gegeben von Sort.java
+        double z;
+        int i,j;
+		int n = a.length;
+		
+		for(i = 0; i < n-1; i++) {
+			
+			for (j = i+1; j < n; j++) {
+				
+				if (a[i] > a[j]) {
+					z = a[i];
+					a[i] = a[j];
+					a[j] = z;
+				}
+			}
+		}	
+			
+	}
 
 }
