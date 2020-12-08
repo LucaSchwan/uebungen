b18f06d22f74c29f564febfce54497ce4e142311
Luca Schwan <schwan.luc@gmail.com>
2020-12-08 16:45:54 +0100

    written BooleanStringHelper.java

---
+++ ue4/BooleanStringHelper.java
@@ -0,0 +1,28 @@
+public class BooleanStringHelper {
+	public static boolean [] parse(String s, char c) {
+		boolean b[];
+	        b = new boolean[s.length()];
+		System.out.println(b);
+		for(int i = 0; i < s.length(); i++) {
+			if(s.charAt(i) == c) {
+				b[i] = true;
+			} else {
+				b[i] = false;
+			}
+		}
+		return b;
+	}
+
+	public static String stringify(boolean[] b, char c1, char c2) {
+		StringBuilder builder = new StringBuilder();
+		for(int i = 0; i < b.length; i++) {
+			if(b[i]) {
+				builder.append(c1);
+			} else {
+				builder.append(c2);
+			}
+		}
+		return builder.toString();
+	}
+}
+
