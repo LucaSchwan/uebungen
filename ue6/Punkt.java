2cac701fe72799dd30d4708ed68d72cef1ecab6d
Partero1 <74548973+Partero1@users.noreply.github.com>
2020-12-20 19:49:13 +0100

    Teil der b) oder so

--- ue6/Punkt.java
+++ ue6/Punkt.java
@@ -1,24 +1,32 @@
 import java.math.*;
 
 public final class Punkt{
-    public  BigDecimal aPunkt;
-    public  BigDecimal ePunkt;
+    public final BigDecimal xStelle;
+    public final BigDecimal yStelle;
 
 
-public Punkt (double anfang, double ende){
-   aPunkt = new BigDecimal(anfang);
-   ePunkt = new BigDecimal(ende);
+    public Punkt (double anfang, double ende){
+    xStelle = new BigDecimal(anfang);
+    yStelle = new BigDecimal(ende);
    
-}
+    }
 
 
-public Punkt (BigDecimal anfang, BigDecimal ende){
-    aPunkt = anfang;
-    ePunkt = ende;
- }
+    public Punkt (BigDecimal anfang, BigDecimal ende){
+        xStelle = anfang;
+        yStelle = ende;
+    }
     
-public String toString(){
-return "(" + aPunkt.toString() + "," + ePunkt.toString();
-}
+    public String toString(){
+    return "(" + xStelle.toString() + "," + yStelle.toString();
+    }
+
+    public BigDecimal abstand(Punkt other){
+        BigDecimal x = other.xStelle.subtract(this.xStelle);
+        BigDecimal y = other.yStelle.subtract(this.yStelle);
+        BigDecimal sqr =x.pow(2).add(y.pow(2));
+    
+        return sqr;
+    }
 
 }
