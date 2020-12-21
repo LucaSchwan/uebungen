3b2fc05406311147cf1286a7d95fcd2facc7e4eb
Partero1 <74548973+Partero1@users.noreply.github.com>
2020-12-21 16:52:22 +0100

    d

--- ue6/Grade.java
+++ ue6/Grade.java
@@ -1,3 +1,5 @@
+import java.math.BigDecimal;
+
 public final class Grade {
     
     public final Punkt p1;    
@@ -34,6 +36,23 @@ public final class Grade {
 
     public String toString(){
         return "Gerade durch (" + p1.xStelle.toString() + "," +p1.yStelle.toString() + ") und (" +p2.xStelle.toString() + "," +p2.yStelle.toString() + ")";
+    }
+
+    private boolean zwischenp1p2(Punkt p0){
+        if(BigDecimalUtility.equalValues(p0.xStelle, p1.xStelle)&&BigDecimalUtility.equalValues(p0.yStelle, p1.yStelle)||BigDecimalUtility.equalValues(p0.xStelle, p2.xStelle)&&BigDecimalUtility.equalValues(p0.yStelle, p2.yStelle)){
+            return true;
+        }
+        else{
+            
+        }
+    }
+
+    private boolean vorp1(Punkt p0){
+
+    }
+
+    private boolean hinterp2(Punkt p0){
+
     }
 
 }
