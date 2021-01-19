a8febee726f923412ebc103290e6df11ae440ccb
Luca Schwan <schwan.luc@gmail.com>
2021-01-19 13:34:51 +0100

    :sparkles: way more updates

--- ue8/Matrix.hs
+++ ue8/Matrix.hs
@@ -4,7 +4,7 @@ isMatrix [_] = True
 isMatrix (e1 : e2 : matrix) = length e1 == length e2 && isMatrix (e2 : matrix)
 
 dimensions :: [[Int]] -> (Int, Int)
-dimensions matrix = if isMatrix matrix then (numCol matrix, numRows matrix) else (-1, -1)
+dimensions matrix = if isMatrix matrix then (numRows matrix, numCol matrix) else (-1, -1)
 
 numRows :: [[Int]] -> Int
 numRows = length
@@ -16,3 +16,10 @@ isQuadratic :: [[Int]] -> Bool
 isQuadratic matrix = if isMatrix matrix then
                        if length matrix == length (matrix !! 1) then True else False
                      else False
+
+getRow :: [[Int]] -> Int -> [Int]
+getRow [] _ = []
+getRow (e : matrix) i = if isMatrix matrix then (e !! i) : (getRow matrix i) else [] 
+
+getCol :: [[Int]] -> Int -> [Int]
+getCol matrix i = if isMatrix matrix then matrix !! i else [] 
