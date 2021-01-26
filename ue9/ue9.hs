5c4610a0d7ab2f95ec467eb42a5805d559f65b4a
Partero1 <74548973+Partero1@users.noreply.github.com>
2021-01-26 19:05:47 +0100

    h

--- ue9/ue9.hs
+++ ue9/ue9.hs
@@ -1,5 +1,9 @@
-data Optional a = Empty | Present a
+data Optional a = Empty | Present a deriving Show
 
-mapOptional::(a ->b) -> Optional a-> Optional b
-mapOptional x Empty = Empty
+mapOptional:: (a -> b) -> Optional a -> Optional b
+mapOptional f Empty = Empty
+mapOptional f o = f o
+
+filterOptional::(a -> Bool) -> Optional a -> Optional a
+filterOptional f Empty = Empty
 
