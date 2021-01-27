3922a9e4f2b398af47a965e68ce4efbf5fde6952
Luca Schwan <schwan.luc@gmail.com>
2021-01-27 09:59:05 +0100

    added Product

--- ue9/ue9.hs
+++ ue9/ue9.hs
@@ -1,9 +1,20 @@
 data Optional a = Empty | Present a deriving Show
+data Product = Article String Int deriving Show
 
-mapOptional:: (a -> b) -> Optional a -> Optional b
+mapOptional :: (a -> b) -> Optional a -> Optional b
 mapOptional f Empty = Empty
-mapOptional f o = f o
+mapOptional f (Present o) = Present (f o)
 
-filterOptional::(a -> Bool) -> Optional a -> Optional a
+filterOptional :: (a -> Bool) -> Optional a -> Optional a
 filterOptional f Empty = Empty
+filterOptional f (Present o) = if f o then Present o else Empty
 
+foldOptional :: (a -> b) -> b -> Optional a -> b
+foldOptional f s Empty = s
+foldOptional f s (Present o) = f o
+
+-- isHumanEatable :: Product -> Bool
+-- isHumanEatable (Product s ) = if s == "Dog Food" then False else True
+
+-- adjustPrice :: Product -> Product
+-- adjustPrice (Product s p) = if p < 1000 then Product(s (p * 2)) else Product(s p)
