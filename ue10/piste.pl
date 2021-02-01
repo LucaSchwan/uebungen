cd89c1f6def561eaee963bc5fb5f8ab22619d1b7
Partero1 <74548973+Partero1@users.noreply.github.com>
2021-02-01 15:46:18 +0100

    a)

--- ue10/piste.pl
+++ ue10/piste.pl
@@ -8,3 +8,17 @@ blau(wiesenalm).
 rot(isskogel).
 %Schwarze Pisten
 schwarz(teufeltal).
+
+%Start Pisten
+start(sonnalm).
+start(teufeltal).
+
+%Pisten Ende
+endetIn(vorkogel, isskogel).
+endetIn(sonnalm, vorkogel).
+endetIn(sonnalm, arbiskoge).
+endetIn(arbiskoge, plattenalm).
+endetIn(teufeltal, wiesenalm).
+endetIn(plattenalm, wiesenalm).
+endetIn(wiesenalm, tal).
+endetIn(isskogel, tal).
